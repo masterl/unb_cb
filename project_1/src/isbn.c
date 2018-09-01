8f48c1139dccd3624a6658797e26016329814750
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2018-09-01 16:24:57 -0700

    Adds normalizing function

--- project_1/src/isbn.c
+++ project_1/src/isbn.c
@@ -1,18 +1,22 @@
+#include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
-bool read_isbn( char isbn[], int const isbn_capacity );
+bool read_isbn( char *isbn, int const isbn_capacity );
+void normalize_isbn( char *normalized, char const *const isbn, int const isbn_capacity );
 
 int main( void )
 {
     int const isbn_capacity = 20;
     char isbn[isbn_capacity];
+    char normalized_isbn[isbn_capacity];
 
 
     return 0;
 }
 
-bool read_isbn( char isbn[], int const isbn_capacity )
+bool read_isbn( char *isbn, int const isbn_capacity )
 {
     int const format_capacity = 32;
     char format_string[format_capacity];
@@ -20,4 +24,20 @@ bool read_isbn( char isbn[], int const isbn_capacity )
     snprintf( format_string, 32, " %%%d[^\n]", isbn_capacity - 1 );
 
     return scanf( format_string, isbn ) == 1;
+}
+
+void normalize_isbn( char *normalized, char const *const isbn, int const isbn_capacity )
+{
+    int index = 0;
+
+    for( int i = 0; isbn[i] && i < ( isbn_capacity - 1 ); ++i )
+    {
+        if( isdigit( isbn[i] ) )
+        {
+            normalized[index] = isbn[i];
+            ++index;
+        }
+    }
+
+    normalized[index] = '\0';
 }
